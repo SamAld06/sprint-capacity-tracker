bdbfd8ea01cb776f8acc751e9e382e884a0be83e
SamAld06 <samgaldred@gmail.com>
2025-12-17 15:28:48 +0000

    Change file order for easier movement across codebase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
-import Image from "next/image";
+import { NavBar } from "@/components/Navbar/navBar";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="min-h-screen flex items-center justify-center">
+    <main className="min-h-screen flex flex-col items-center justify-center gap-4">
+      <NavBar />
       <h1 className="text-2xl font-bold">Sprint Capacity Tracker</h1>
+      <Link href="/dashboard">
+        <button className="px-4 py-2 bg-blue-500 text-white rounded">
+          Go to Dashboard
+        </button>
+      </Link>
     </main>
   );
 }
---
+++ src/components/Navbar/navBar.tsx
@@ -0,0 +1,5 @@
+export const NavBar = () => {
+  return (
+    <header>SCT</header>
+  );
+};
