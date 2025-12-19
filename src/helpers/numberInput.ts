27b9302b4610db77efc0ef674b171f9cb9d6d1e1
SamAld06 <samgaldred@gmail.com>
2025-12-19 17:37:33 +0000

    SCT-20 Initial creation and styling for inputs

---
+++ src/helpers/numberInput.ts
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+// export const numberInputChange = (currentValue: number) => {
+//     const [value, setValue] = useState(currentValue);
+
+//     const onChange = (e) => {
+//         const newValue = e.target.value;
+//         if (/^\d*$/.test(newValue)) {
+//             setValue(newValue);
+//         }
+//     }
+//     return {value, onChange};
+// }
