d97886d785a54a2bd1d4578b192b0e665037f9d9
SamAld06 <samgaldred@gmail.com>
2026-01-08 11:30:49 +0000

    SCT-7 Fix typing and add groupcode to each table

--- backend/src/seed.ts
+++ backend/src/seed.ts
@@ -4,10 +4,34 @@ const db = new sqlite3.Database("mydatabase.db");
 
 db.serialize(() => {
     console.log("Seeding database")
-    db.run("DELETE FROM sprint")
-    db.run(
-        `
-        INSERT INTO sprint (
+    db.exec('DELETE FROM sprint;')
+    db.exec(`INSERT INTO capacity (
+            groupCode,
+            sprintId,
+            name,
+            workAssigned,
+            workCompleted,
+            averagePerMd
+        ) VALUES
+            ('t3stGr0up1', 1, 'tester', 7, 5, 0.5),
+            ('t3stGr0up1', 2, 'tester', 5, 3, 0.429),
+            ('t3stGr0up1', 3, 'tester', 4, 4, 0.667);`);
+    db.exec(`INSERT INTO availability (
+            groupCode,
+            sprintId,
+            name,
+            workingDays,
+            outOfOffice,
+            releases,
+            fridayProjects,
+            maintenance,
+            md
+        ) VALUES
+            ('t3stGr0up1', 1, 'tester', 10, 0, 0, 0, 0, 10),
+            ('t3stGr0up1', 2, 'tester', 7, 2, 0, 1, 0, 7),
+            ('t3stGr0up1', 3, 'tester', 6, 3, 0, 0, 1, 6);`);
+    db.exec(`INSERT INTO sprint (
+            groupCode,
             planned,
             added,
             removed,
@@ -15,10 +39,8 @@ db.serialize(() => {
             totalMd,
             plannedCompletedDifference
         ) VALUES
-            (28, 2, 1, 24, 18, 0.154),
-            (30, 0, 2, 25, 20, 0.182),
-            (35, 5, 0, 28, 22, 0.222)
-        `
-    );
+            ('t3stGr0up1', 28, 2, 1, 24, 18, 0.154),
+            ('t3stGr0up1', 30, 0, 2, 25, 20, 0.182),
+            ('t3stGr0up1', 35, 5, 0, 28, 22, 0.222);`);
     console.log("Seeding complete");
 });
