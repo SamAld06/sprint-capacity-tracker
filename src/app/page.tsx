3323b094f3178402d75d706bd713d632eef6a9fd
SamAld06 <samgaldred@gmail.com>
2026-01-09 12:05:27 +0000

    SCT-7 Remove unused code from main page file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,8 @@
 import { NavBar } from "@/components/navbar/navBar";
 import Link from "next/link";
 import styles from './styles.module.css'
-import { useEffect, useState } from "react";
-import { sprint } from "@/types/sprint";
-import { sprintDetailsService } from "@/services/sprintDetailsService";
 
 export default function Home() {
-  const [sprint, setSprint] = useState<sprint[]>([])
-  const [err, setErr] = useState<string | null>(null)
-  const [loading, setLoading] = useState(true)
-  useEffect(() => {
-    const fetchSprints = async () => {
-      try {
-        const data = await sprintDetailsService.getAll()
-        setSprint(data)
-      } catch (err) {
-        setErr((err as Error).message)
-      } finally {
-        setLoading(false)
-      }
-    };
-    fetchSprints();
-  }, []);
   return (
     <main className={styles.root}>
       <NavBar/>
@@ -38,9 +19,6 @@ export default function Home() {
           <button className={styles.button}>Join group</button>
         </Link>
       </div>
-      {sprint.map((sprint) => (
-        <p>{sprint.planned}</p>
-      ))}
     </main>
   );
 }
