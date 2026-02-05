e9ae8f010ac0802cf11c16c619c06f7ddf3dda7a
SamAld06 <samgaldred@gmail.com>
2026-02-05 16:55:03 +0000

    SCT-28 Move supabase url to env

--- backend/src/supabaseclient.ts
+++ backend/src/supabaseclient.ts
@@ -1,3 +1,3 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabase = createClient('https://wcyxqkhptsacqfcscwgt.supabase.co', process.env.SUPABASE_KEY)
+const supabase = createClient(process.env.SUPABASE_LINK, process.env.SUPABASE_KEY)
